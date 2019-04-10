8d2c45ad54147c225cb1f2a38d8c98c6ac8a7232
Alexeis Vladimir Carrillo Pinaya <acarrillo@mail.rua.net>
2019-04-10 18:57:20 -0400

    Mapas funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AlertService } from './../services/util/alert.service';
+import { NavParamService } from './../services/nav-param.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { MdlCliente } from '../modelo/mdlCliente';
 import { SesionService } from '../services/sesion.service';
+import { ToastService } from '../services/util/toast.service';
+declare var google;
 
 @Component({
   selector: 'app-home',
@@ -11,9 +15,18 @@ import { SesionService } from '../services/sesion.service';
 })
 export class HomePage implements OnInit {
   cliente: MdlCliente;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  latitud: string;
+  longitud: string;
+  paginaRetorno: string;
   constructor(
     public sesionService: SesionService,
-    public navController: NavController
+    public navParam: NavParamService,
+    public navCtrl: NavController,
+    public toastCtrl: ToastService,
+    public modalController: ModalController,
+    public alertController: AlertService
   ) { }
 
   ngOnInit() {
@@ -24,9 +37,40 @@ export class HomePage implements OnInit {
             if (cliente) {
               this.cliente = cliente;
             } else {
-              this.navController.navigateRoot('/login');
+              this.navCtrl.navigateRoot('/login');
             }
           });
       });
+      this.cargarMapa();
+  }
+  cargarMapa() {
+    const myLatlng = { lat: -16.4978888, lng: -68.1314424};
+    const mapOptions = {
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false,
+      streetViewControl: true,
+      fullScreenControl: false,
+      center: myLatlng
+    };
+    this.latitud = myLatlng.lat.toString();
+    this.longitud = myLatlng.lng.toString();
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    let marker = new google.maps.Marker
+    ({
+      position: myLatlng,
+      map: this.map,
+      draggable: true,
+      title: 'Mueveme'
+    });
+    marker.addListener('dragend', () => {
+      console.log(JSON.stringify(marker.getPosition()));
+      const objStr: string = JSON.stringify(marker.getPosition());
+      const obj = JSON.parse(objStr);
+      // window.alert(JSON.stringify(marker.getPosition()));
+      this.latitud = obj.lat;
+      this.longitud = obj.lng;
+    });
+    // {"lat":-16.498217987944532,"lng":-68.13232216455685}
   }
 }
